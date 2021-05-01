33f6b341a3b1284ae4f746c04ca37e62f45f5aac
Valentina Kim <vvkim86@gmail.com>
2021-05-01 11:01:28 -0400

    Initial commit

---
+++ tests/test_webdriver.py
@@ -0,0 +1,53 @@
+from source.finding_elements import *
+
+print("Execution starting ...")
+
+# print("Scenario 1: WebDriver methods, properties, WebElements methods (input fields)")
+# url_inputs = "https://www.seleniumeasy.com/test/basic-first-form-demo.html"
+# open_website(url_inputs)
+# back_forward()
+# get_total_input_fields()
+#
+# print("# Scenario 2: Handling CheckBox")
+# url_checkbox = "https://www.seleniumeasy.com/test/basic-checkbox-demo.html"
+# open_website(url_checkbox)
+# # create steps to test checkbox using selenium
+# checkbox_test()
+#
+# print("# Scenario 3 : working with multiple elements, ecommerce website")
+# # go to this website and search the product
+# print("scenario 3 started")
+# website = "http://automationpractice.com/index.php"
+# open_website(website)
+# ecommerce_search()
+# print("scenario 3 completed")
+#
+# print("# Scenario 4: amazon example, find_elements")
+# # amazon_example()
+#
+#
+# print("# Scenario 5: drop down list")
+# drop_down_select()
+#
+# print("# Scenario 6: drop down multi select methods")
+# drop_down_multi_select()
+#
+# print("# Scenario 7: handling the js alert")
+# switch_to_alert()
+#
+# print("# Scenario 8: handling the popup window")
+# switch_to_window()
+#
+# print("Scenario 9: explicit wait cases")
+# explicit_wait_methods()
+
+# print("Scenario 10: Mouse actions - drag and drop")
+drag_drop_action()
+# drag_drop_action3()
+
+# print("Scenario 11: mouse moving action")
+# move_mouse_action()
+
+print("closing the browser")
+close_browser()
+print("Steps are completed!")
